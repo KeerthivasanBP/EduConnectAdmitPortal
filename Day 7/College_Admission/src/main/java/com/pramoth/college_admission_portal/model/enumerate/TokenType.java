40e7c1d5262f4635718c9ac5f42a52fbbbf416cd
KeerthivasanBP <115928510+KeerthivasanBP@users.noreply.github.com>
2024-11-22 17:43:13 +0530

    Delete Day 7 directory

--- Day 7/College_Admission/src/main/java/com/pramoth/college_admission_portal/model/enumerate/TokenType.java
+++
@@ -1,5 +0,0 @@
-package com.pramoth.college_admission_portal.model.enumerate;
-
-public enum TokenType{
-      BEARER
-}
--- Day 7/College_Admission/src/main/java/com/pramoth/college_admission_portal/repository/ContactdetailRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pramoth.college_admission_portal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pramoth.college_admission_portal.model.Contactdetails;
-
-public interface ContactdetailRepo extends JpaRepository<Contactdetails,String>{
-    
-}
